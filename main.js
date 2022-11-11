925fd001ed6e9b5cf379a6a22ddcb851ee8b89a8
Will Hawkins <whh8b@obs.cr>
2022-11-11 17:36:34 -0500

    Cleanup and add documentation.

--- main.js
+++ main.js
@@ -1,67 +1,102 @@
-//import { readFileSync, writeFileSync } from 'fs'
-//import { join, dirname } from 'path'
-
-const pako = require('pako')
 const path = require('path')
 const fs = require('fs')
-
 const UPNG = require('./UPNG.js');
+const assert = require('assert');
 
-function encode() {
-    console.log(UPNG)
-    const haystack = fs.readFileSync(path.join(path.dirname('./'), "taylor.png"))
-    const needle = fs.readFileSync(path.join(path.dirname('./'), "hidden.png"))
+/*
+ * The image at `haystackFilePath` should be a 16-bit-depth RGB PNG. The needle should be encoded
+ * in a black-and-white image (message in black, background in white) as a 8-bit-depth GRAY PNG.
+ * The needle will be encoded in the file written to encodedFilePath in a format suitable for
+ * deciphering with the decode function.
+ */
+function encode(haystackFilePath, needleFilePath, encodedFilePath) {
 
+    let haystack = null
+    let needle = null
+    try {
+        haystack = fs.readFileSync(haystackFilePath)
+        needle = fs.readFileSync(needleFilePath)
+    } catch (error) {
+        return (false, error)
+    }
+
+    assert(haystack != null)
+    assert(needle != null)
 
     const haystackImage = UPNG.decode(haystack)
     const haystackData = haystackImage.data
     const needleImage = UPNG.decode(needle)
     const needleData = needleImage.data
 
-    // for every pixel in 
-    for (var i = 0; i<haystackData.byteLength; i+=3) {
-        const needleValue = needleData[i/3]
-        if (needleValue > 10) {
-            console.log(`Pixel at ${i/3} is on.`)
+    // for the red channel of every pixel in the haystack, compare the 
+    // black/white-ness of the pixel in the needle. If the pixel in the needle
+    // is not white, it represents part of the signal. So, we set the red
+    // channel value to its nearest even value. Otherwise, we make sure that
+    // value is to the nearest odd value. This is our signal to the decoder
+    // and one that does not obscure the data too much!
+    for (var i = 0; i < haystackData.byteLength; i += 3) {
+        const needleValue = needleData[i / 3]
+        if (needleValue < 255) {
             // Make the pixel even!
-            if (haystackData[i] % 2) {
-                haystackData[i]++;
-            }
+            haystackData[i] &= 0xfe
         } else {
             // Make the pixel odd!
-            if (!(haystackData[i] % 2)) {
-                haystackData[i]--;
-            }
+            haystackData[i] |= 0x1
         }
     }
 
-    var encodedImageFrames =  Array(1)
+    var encodedImageFrames = Array(1)
     encodedImageFrames[0] = haystackData
-    console.log(haystackImage)
     const encoded = UPNG.encodeLL(encodedImageFrames, haystackImage.width, haystackImage.height, 3, 0, 16)
-    fs.writeFileSync(path.join(path.dirname('./'), "testing.png"), Buffer.from(encoded))
+    try {
+        fs.writeFileSync(encodedFilePath, Buffer.from(encoded))
+    } catch (error) {
+        return (false, error)
+    }
+
+    return (true, null)
 }
 
-function decode() {
-    console.log(UPNG)
-    const encoded = fs.readFileSync(path.join(path.dirname('./'), "testing.png"))
+function isEven(eoro) {
+    return (eoro % 2) == 0
+}
+
+function decode(encodedFilePath, decodedFilePath) {
+    const encoded = fs.readFileSync(encodedFilePath)
 
     const encodedImage = UPNG.decode(encoded)
     const encodedImageData = encodedImage.data
     const decodedImageData = new Uint8Array(encodedImageData.byteLength)
 
-    // for every pixel in 
-    for (var i = 0; i<encodedImageData.byteLength; i+=3) {
+    // for the red channel of every pixel in the encoded file, see
+    // if its value is even. If it is, then that means the pixel is part
+    // of the signal.
+    for (var i = 0; i < encodedImageData.byteLength; i += 3) {
         const value = encodedImageData[i]
-        if (!(value % 2)) {
+        if (isEven(value)) {
             decodedImageData[i] = 255;
         }
     }
 
-    var decodedImageFrames =  Array(1)
+    var decodedImageFrames = Array(1)
     decodedImageFrames[0] = decodedImageData
     const decoded = UPNG.encodeLL(decodedImageFrames, encodedImage.width, encodedImage.height, 3, 0, 8)
-    fs.writeFileSync(path.join(path.dirname('./'), "decoded.png"), Buffer.from(decoded))
+    fs.writeFileSync(decodedFilePath, Buffer.from(decoded))
 }
-encode()
-decode()
+
+var taylorFilePath = path.join(path.dirname('./'), "original.png")
+var hiddenFilePath = path.join(path.dirname('./'), "secret.png")
+var encodedFilePath = path.join(path.dirname('./'), "encoded.png")
+var decodedFilePath = path.join(path.dirname('./'), "decoded.png")
+var encode_result, encode_error = encode(taylorFilePath, hiddenFilePath, encodedFilePath)
+if (encode_result == false) {
+    console.log(`Error encoding: ${encode_error}`)
+} else {
+    console.log("Encoding successful.")
+}
+var decode_result, decode_error = decode(encodedFilePath, decodedFilePath)
+if (decode_result == false) {
+    console.log(`Error decoding: ${decode_error}`)
+} else {
+    console.log("Decoding successful.")
+}
